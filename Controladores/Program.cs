daedab06a0b0e2bdfca2f8c15c1a991470fc18bc
eduballesteros0 <ballesterospereze@gmail.com>
2023-10-25 11:04:59 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,5 @@
+﻿/*Escribe un programa que solicite un número entero mayor que 0 al usuario, lo lea de la consola y calcule la siguiente suma: 1+2+3...+n-1+n.
+Por ejemplo, si el número es 9, calculará 1+2+3+4+5+6+7+8+9.
+
+El método que solicite el número deberá estar definido en un servicio. El método mostrará el mensaje de petición de número, recogerá el número escrito en la consola y lo devolverá al método main.
+El método que calcule la suma y muestre por consola el resultado deberá estar definido también en un servicio.*/
